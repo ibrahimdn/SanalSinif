2e3bb1bd710c4898985b918ddf51ea4621aaea63
ibrahim <ibrhmdn@ibrahim-Mac.local>
2017-11-05 23:19:47 +0300

    Initial Commit

---
+++ Sanal Sınıf/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sanal Sınıf
+//
+//  Created by ibrahim on 5.11.2017.
+//  Copyright © 2017 ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sanal SınıfTests/Sanal_S_n_fTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sanal_S_n_fTests.swift
+//  Sanal SınıfTests
+//
+//  Created by ibrahim on 5.11.2017.
+//  Copyright © 2017 ibrahim. All rights reserved.
+//
+
+import XCTest
+@testable import Sanal_S_n_f
+
+class Sanal_S_n_fTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
